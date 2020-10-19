c2fb0ed76bcae961a64f5928f75c09972f58f590
Clémence Mayaux <clemence.mayaux@epfl.ch>
2020-10-19 21:46:53 +0200

    Ajout de computer_player.py 100% aléatoire

---
+++ computer_player.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 21:29:46 2020
+
+@author: clem7
+
+This script will try to guess the mysterious number prepared
+by guess_my_number.GuessMachine
+
+It's strategy is to try random numbers'
+"""
+
+import random
+from guess_my_number import MIN,MAX,GuessMachine
+
+if __name__=='__main_':
+    guess_machine=GuessMachine()
+    while True:
+        attempt= random.randint(MIN,MAX)
+        result = guess_machine.guess(attempt)
+        print('tried %d : %s' % (attempt,result))
+        if result == 'found' :
+            
+            print('Finished in %d attempts.' % guess_machine.number_of_attempt)
+            break
+
